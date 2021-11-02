2d6b4c9d4dd76d4c93c7d622ec3b8009d7ab626e
yongbeomkwak <yongbeomkwak@gmail.com>
2021-11-02 15:37:27 +0900

    Regex

---
+++ 2021 KAKAO_숫자_문자열과_영단어.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl "\n"
+
+
+
+int solution(string s) {
+    //regex_replace(문자열,regex(패턴),대체할 문자열)
+    s = regex_replace(s, regex("zero"), "0");
+    s = regex_replace(s, regex("one"), "1");
+    s = regex_replace(s, regex("two"), "2");
+    s = regex_replace(s, regex("three"), "3");
+    s = regex_replace(s, regex("four"), "4");
+    s = regex_replace(s, regex("five"), "5");
+    s = regex_replace(s, regex("six"), "6");
+    s = regex_replace(s, regex("seven"), "7");
+    s = regex_replace(s, regex("eight"), "8");
+    s = regex_replace(s, regex("nine"), "9");    
+    return stoi(s);
+}
--- README.md
+++ README.md
@@ -3,3 +3,12 @@
 ## Setting
 - [#include <bits/stdc++.h>](https://raeyoungii.github.io/bits-stdc++.h/)
 -  [climits](https://docs.microsoft.com/ko-kr/cpp/c-language/cpp-integer-limits?view=msvc-160)
+
+
+1.  2021 KAKAO_신규_이이디_추천
+    -   비트연산 이용 대소문자 변호나
+    -   strchr(문자열,문자) :포함 여부 
+
+2.  2021 KAKAO_숫자_문자열과_영단어
+    -   regex_replace(문자열,regex(패턴),대체할 문자열)
+    -   stoi(string) -> string -> int
