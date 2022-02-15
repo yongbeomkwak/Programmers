22f21978cdfe24069f9c63f44cbf1b753dcd17f3
yongbeomkwak <yongbeomkwak@gmail.com>
2022-02-15 18:13:32 +0900

    U

---
+++ 같은 숫자는 싫어.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr) 
+{
+    arr.erase(unique(arr.begin(), arr.end()),arr.end());
+
+    vector<int> answer = arr;
+    return answer;
+    
+
+    return answer;
+}
