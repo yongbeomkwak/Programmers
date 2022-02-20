37a26c86195f920937538dae855642417b94df95
yongbeomkwak <yongbeomkwak@gmail.com>
2022-02-20 21:11:50 +0900

    N

--- Lv2/Readme.md
+++ Lv2/Readme.md
@@ -2,3 +2,6 @@
 
 -   게임 맵 최단거리
     -   0-1 BFS
+-   JadenCase 문자열 만들기
+    -   toupper(char): 대문자 변환
+    -   tolower(char): 소문자 변환
---
+++ Lv2/숫자의 표현.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int solution(int n)
+{
+    int answer = 0;
+
+    // int tmp=0;
+
+    for (int i = 1; i <= n; i++)
+    {
+        int sum = 0;
+        for (int j = i; i <= n; j++)
+        {
+            sum += j;
+            if (sum == n)
+            {
+                answer++;
+                break;
+            }
+            else if (sum > n)
+                break;
+        }
+    }
+    return answer;
+}
