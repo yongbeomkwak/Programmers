e2c9e4831204ae1e60b706272617de20ef530906
yongbeomkwak <yongbeomkwak@gmail.com>
2022-05-24 23:44:16 +0900

    Tu

--- 2019 KAKAO_Self튜플.cpp
+++ 2019 KAKAO_Self튜플.cpp
@@ -1,83 +1,44 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-#define endl "\n"
-unordered_map<string, int> _m;
-
-void insert(vector<int> &ans, vector<string> &s)
-{
-    for (const string &str : s)
-    {
-        string temp;
-        for (const char &c : str)
-        {
-            if (isdigit(c)) //숫자면
-            {
-                temp.push_back(c); // temp에 추가
-            }
-        }
-        if (!temp.empty() && _m[temp] == 0) // temp가 비어있지 않고 한번도 추가 안됬으면 추가
-        {
-            _m[temp] = 1;
-            int conv = stoi(temp); // convert
-            ans.push_back(conv);   //추가
-        }
-    }
-}
-
-bool comp(string str1, string str2)
-{
-    return str1.length() < str2.length();
-}
-
-vector<string> split(string s, char delim) // split
-{
-    vector<string> ret;
-    stringstream ss(s);
-    string temp;
-    while (getline(ss, temp, delim))
-    {
-
-        ret.push_back(temp);
-    }
-    return ret;
-}
-
-void print(vector<string> v)
-{
-    for (const string &str : v)
-    {
-        cout << str << " ";
-    }
-}
-
+#define MAX 100001
+int cnt[MAX] = {
+    0,
+};
+unordered_set<int> ss;
+int len;
 vector<int> solution(string s)
 {
     vector<int> answer;
-    s.erase(s.length() - 1, 1);
-    s.erase(0, 1);
-    //양쪽 끝 제거
-    vector<string> v0 = split(s, '{');
-    //위에서 { 를 제거하고
-    string s0; //담아둘 s0
-    for (const string &str : v0)
+    bool flag1 = false;
+    bool flag2 = false;
+    string tmp = "";
+    for (int i = 2; i < s.length() - 1; i++)
     {
-        for (const char &c : str)
+        if (isdigit(s[i]))
+            tmp += s[i];                //숫자이면 tmp에 더함
+        if (s[i] == ',' || s[i] == '}') //만약 ,이거나 }이고
         {
-            s0.push_back(c); // { 를 제하고 s0에 넣음
+            if (!tmp.empty()) // tmp가 비어있지 않으면
+            {
+                int itmp = stoi(tmp); //해당문자열 int로
+                ss.insert(itmp);      //셋에 삽입
+                cnt[itmp]++;          //해당 인덱스에 해당하는 카운트 갯수 증가
+                tmp.clear();          //비워주고
+            }
         }
     }
-    vector<string> s1 = split(s0, '}'); //마찬가지로 }를 제거
-    sort(s1.begin(), s1.end(), comp);   //길이가 짧은게 앞으로
-    vector<vector<string> > res;
-    for (int i = 0; i < s1.size(); i++)
+    len = ss.size(); //최대갯수는 len부터  ~1까지
+    while (len >= 1)
     {
-        res.push_back(split(s1[i], ',')); // ,로 구분하여 넣어줌
+        for (auto it = ss.begin(); it != ss.end(); it++)
+        {
+            const int idx = *it; //해당 값의 카운트가 현재 len과 같다면 삽입
+            if (len != cnt[idx])
+                continue;
+            answer.push_back(idx);
+            len--;
+        }
     }
-    for (vector<string> &s : res)
-    {
-        insert(answer, s); //최종 작업
-    }
-
     return answer;
 }
