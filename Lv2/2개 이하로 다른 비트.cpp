2cb147be6671b3eb88bdc4323fb22da1d784e3ff
yongbeomkwak <yongbeomkwak@gmail.com>
2022-05-27 23:40:22 +0900

    ch

--- Lv2/2개 이하로 다른 비트.cpp
+++ Lv2/2개 이하로 다른 비트.cpp
@@ -12,6 +12,14 @@ vector<long long> solution(vector<long long> numbers)
             answer.push_back(n + 1);
         else
         {
+            //홀수는 최상위 비트 바로 직전 자리를 올려주면
+            //최상위 비트도 변하고 ,직전 비트도 변하므로 2개가 변함
+            /*
+            ex)
+            0111 (7)  왼쪽에서 두번째 (최상위)직전 비트 +1
+            1011 (11) 만족
+         
+            */ 
             long long bit = 1;
             while (1)
             {
@@ -19,8 +27,8 @@ vector<long long> solution(vector<long long> numbers)
                     break;
                 bit <<= 1; //계속 왼쪽으로 밀면서 틀릴 때 까지
             }
-            bit >>= 1;
-            answer.push_back(n + bit);
+            bit >>= 1; //한번만 오른쪽으로 옮겨 최상위 직전비트 위치를 만든후 
+            answer.push_back(n + bit); //더함
         }
     }
 
