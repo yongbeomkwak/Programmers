01f88524cb4ca11397a88186fb34249153aaa63d
yongbeomkwak <yongbeomkwak@gmail.com>
2022-06-19 00:59:25 +0900

    SQRT

---
+++ Lv2/숫자블록.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define MAX 10000000
+int task(long long n)
+{
+    int end = sqrt(n);
+    int ret = 1;
+    for (int i = 2; i <= end; i++)
+    {
+        int div = n / i;
+        if (n % i == 0 && div <= MAX)
+        {
+            ret = div;
+            break;
+        }
+    }
+    return ret;
+}
+
+vector<int> solution(long long begin, long long end)
+{
+    vector<int> answer(end - begin + 1, 0);
+
+    // 3 9
+    // blocks=6
+    // 0 1 2 3 4 5 6
+    // 3 4 5 6 7 8 9
+    // 0 0 0 3 0 0 3
+    // 0 0 0 3 0 4 3
+
+    int idx = 0;
+
+    for (long long start = begin; start <= end; start++)
+    {
+        if (start != 1)
+            answer[idx] = task(start);
+        idx++;
+    }
+
+    return answer;
+}
