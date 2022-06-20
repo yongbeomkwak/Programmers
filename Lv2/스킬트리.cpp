2093e92121e69ba162e9823401adfae9d5e1c3df
yongbeomkwak <yongbeomkwak@gmail.com>
2022-06-20 20:41:23 +0900

    skill

--- Lv2/스킬트리.cpp
+++ Lv2/스킬트리.cpp
@@ -2,42 +2,37 @@
 
 using namespace std;
 
-int solution(string skill, vector<string> skill_trees)
-{
+int solution(string skill, vector<string> skill_trees) {
     int answer = 0;
-
-    for (string &s : skill_trees)
+    
+    for(string& s:skill_trees)
     {
-        vector<bool> used(skill.size(), false); //스킬 배움 여부
-        bool flag = true;                       //통과 플래그
-        for (const char &c : s)
+        unordered_map<char,int> um;
+        for(int i=0;i<s.length();i++)
         {
-            if (skill.find(c) != string::npos) //만약 스킬 트리에 선행스킬이 있다면
-            {
-                int idx = skill.find(c); //해당 인덱스를 찾고
-                if (idx == 0)            // 0이면 기초스킬이 므로
-                {
-                    used[0] = true; //배움
-                }
-                else //이닐 경우 선행 스킬 확인 해야함
-                {
-                    for (int i = 0; i < idx; i++) //현재 스킬 이전 선행스킬들 확인
-                    {
-                        if (!used[i]) //선행 스킬들 중 하나라도 배운게 없다면
-                        {
-                            cout << s << endl;
-                            flag = false; //잘못된 스킬트리
-                            break;
-                        }
-                    }
-                    if (!flag)
-                        break;        //스킬트리 뒷 내용 볼필요 없음
-                    used[idx] = true; //이전에 이상 없다면 배울 수 있음
-                }
-            }
+            um[s[i]]=i+1; //해당 스킬의 위치를 기억 
         }
-        if (flag)
-            answer++; //끝까지 문제 없다면 증가
+        bool flag=true;
+        int prev=-1;  //이전 스킬 위치
+        for(const char& c:skill)
+        {
+            if(um[c]==0) //0이면 스킬트리에 없다는 뜻 
+            {
+                prev=s.length()+1;//가장 큰 값
+                continue;
+            }
+            
+            if(prev>um[c]) //이전 스킬이 현재 스킬보다 크다(나중에 찍혔다)
+            {
+                flag=false; //실패 
+                break;
+            }
+            prev=um[c];
+            
+            
+        }
+        if(flag) answer++; //성공이면 증가
+        
     }
     return answer;
 }
